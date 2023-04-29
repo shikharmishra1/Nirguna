a1e6bda459af2aba2f75389025e36d625fd4c47c
shikh <shikh@HP>
2023-04-29 22:53:54 +0530

    created AST (Abstract Syntax tree) definitions

---
+++ NirgunaLang/AST.ts
@@ -0,0 +1,40 @@
+export enum AstNodeType {
+    Program = "Program",
+    BinaryExpression = "BinaryExpression",
+    Identifier = "Identifier",
+    NumericLiteral = "Literal",
+    VariableDeclaration = "VariableDeclaration",
+  }
+  
+  export interface AstNode {
+    type: AstNodeType;
+  }
+  
+  export interface ProgramNode extends AstNode {
+    type: AstNodeType.Program;
+    body: AstNode[];
+  }
+  
+  export interface BinaryExpressionNode extends AstNode {
+    type: AstNodeType.BinaryExpression;
+    operator: string;
+    left: AstNode;
+    right: AstNode;
+  }
+  
+  export interface IdentifierNode extends AstNode {
+    type: AstNodeType.Identifier;
+    name: string;
+  }
+  
+  export interface NumericLiteralNode extends AstNode {
+    type: AstNodeType.NumericLiteral;
+    value: number;
+  }
+  
+  export interface VariableDeclarationNode extends AstNode {
+    type: AstNodeType.VariableDeclaration;
+    name: string;
+    value: AstNode;
+  }
+  
