a3e0a4be337f8ea9c1e4dce7a42f9f28affa7000
shikh <shikh@HP>
2023-05-07 17:59:26 +0530

    added support for interpreting from file

--- NirgunaLang/main.ts
+++ NirgunaLang/main.ts
@@ -1,17 +1,28 @@
 import {parse} from './parser'
 import * as readline from 'readline';
-import {evaluate} from './runtime/interpretor'
+import {evaluate} from './runtime/interpreter'
 import Environment from './runtime/environment';
+import fs from 'fs'
+
 import { MK_BOOL, MK_NUMBER, NumericValueNode, ValueNodeType } from './runtime/values';
 
 const env = new Environment
+
+function run2 (file:string)
+{
+  const input =  fs.readFileSync(file, 'utf-8');
+
+  const program = parse(input);
+  const result = evaluate(program, env);
+  console.log(result);
+}
 
 function run() {
   rl.question(">", function(input) {
     const program = parse(input);
     const result = evaluate(program, env);
     console.log(JSON.stringify(program, null, 2));
-    console.log(result);
+    
     if(!input || input.includes("exit"))
     {
       process.exit(0);
@@ -24,5 +35,5 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
-run();
+run2('./test/test.nirguna');
+//run();
